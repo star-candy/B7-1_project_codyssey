a4dd934b9980701711b761ac86c525e18c5ac2bf
jhj9109 <jhj91_09@naver.com>
2026-08-06 19:37:27 +0900

    eat(auth): JWT 생성 함수 구현
    - python-jose를 활용한 create_access_token 함수 구현
    - settings에서 secret_key와 algorithm값 불러와 사용

--- auth/security.py
+++ auth/security.py
@@ -1,4 +1,8 @@
+from datetime import datetime, timedelta, timezone
+from jose import jwt
 import bcrypt
+
+from core.config import settings
 
 def get_password_hash(password: str) -> str:
     """비밀번호를 bcrypt 알고리즘을 사용해 암호화(단방향 해시)하여 반환합니다."""
@@ -11,3 +15,17 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
         return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
     except Exception:
         return False
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    """주어진 데이터(페이로드)를 바탕으로 JWT Access Token을 생성합니다."""
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(datetime.timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    
+    # 토큰 만료 시간 추가
+    to_encode.update({"exp": expire})
+    # 시크릿 키와 알고리즘을 사용해 토큰 생성
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
+    return encoded_jwt
