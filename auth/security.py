2ae97d137d6538042252ce9f053855907e62aea9
jhj9109 <jhj91_09@naver.com>
2026-08-06 20:28:08 +0900

    hotfix import Optional 추가

--- auth/security.py
+++ auth/security.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta, timezone
+from typing import Optional
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
